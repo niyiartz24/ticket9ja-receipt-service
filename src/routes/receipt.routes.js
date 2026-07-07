6d7d58b4814efa609693707eb4de917857e19027
niyiartz24 <emmyniyi1208@gmail.com>
2026-07-07 13:12:52 +0100

    Fix receipt search route

--- src/routes/receipt.routes.js
+++ src/routes/receipt.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const receiptController = require("../controllers/receipt.controller");
 
-router.post("/search", receiptController.search);
+router.post("/search", receiptController.searchReceipt);
 
 router.get("/:receiptId", receiptController.getReceipt);
 
