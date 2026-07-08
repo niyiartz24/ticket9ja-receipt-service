f71b7c65a849d9449374e4897d0dda84ca5296e5
niyiartz24 <emmyniyi1208@gmail.com>
2026-07-08 10:10:06 +0100

    Use compatible Chromium and Puppeteer versions

--- src/services/pdf.service.js
+++ src/services/pdf.service.js
@@ -3,29 +3,22 @@ const puppeteer = require("puppeteer-core");
 
 exports.generatePDF = async (html) => {
     const browser = await puppeteer.launch({
-        executablePath: await chromium.executablePath(),
         args: chromium.args,
-        headless: true
+        executablePath: await chromium.executablePath(),
+        headless: true,
     });
 
     try {
         const page = await browser.newPage();
 
         await page.setContent(html, {
-            waitUntil: "networkidle0"
+            waitUntil: "networkidle0",
         });
 
         return await page.pdf({
             format: "A4",
             printBackground: true,
-            margin: {
-                top: "20px",
-                right: "20px",
-                bottom: "20px",
-                left: "20px"
-            }
         });
-
     } finally {
         await browser.close();
     }
