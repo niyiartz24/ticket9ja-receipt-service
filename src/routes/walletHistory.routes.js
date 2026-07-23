d310e63d9dcefbf7e14ffd658b3af1aeee89c096
niyiartz24 <emmyniyi1208@gmail.com>
2026-07-23 08:50:24 +0100

    wallet fix

--- src/routes/walletHistory.routes.js
+++ src/routes/walletHistory.routes.js
@@ -3,8 +3,7 @@ const router = require("express").Router();
 const controller =
 require("../controllers/walletHistory.controller");
 
-const auth =
-require("../middleware/auth.middleware");
+const auth = require("../auth/auth.middleware");
 
 router.get(
     "/",
