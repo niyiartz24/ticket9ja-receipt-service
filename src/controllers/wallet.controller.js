a26044507ed3ff4def94ad9ce4aced1a02c0ad20
niyiartz24 <emmyniyi1208@gmail.com>
2026-07-28 13:27:53 +0100

    wallet fix

--- src/controllers/wallet.controller.js
+++ src/controllers/wallet.controller.js
@@ -1,29 +1,75 @@
 const organizationWallet = require("../services/organizationWallet.service");
+const collegeWallet = require("../services/collegeWallet.service");
+const departmentWallet = require("../services/departmentWallet.service");
 
 exports.getWallet = async (req, res) => {
+
     try {
 
-        const wallet =
-            await organizationWallet.details(
-                req.user.organizationId
-            );
+        let wallet;
+
+        switch (req.user.role) {
+
+            case "ORGANIZATION_ADMIN":
+
+                wallet = await organizationWallet.details(
+                    req.user.organizationId
+                );
+
+                break;
+
+            case "COLLEGE_ADMIN":
+
+                wallet = await collegeWallet.details(
+                    req.user.collegeId
+                );
+
+                break;
+
+            case "DEPARTMENT_ADMIN":
+
+                wallet = await departmentWallet.details(
+                    req.user.departmentId
+                );
+
+                break;
+
+            default:
+
+                throw new Error("Unsupported role.");
+
+        }
 
         res.json({
+
             success: true,
+
             balance: Number(wallet.availableBalance),
+
             pending: Number(wallet.pendingBalance),
+
             reserved: Number(wallet.reservedBalance),
+
             withdrawn: Number(wallet.withdrawnBalance),
+
             totalRevenue: Number(wallet.totalRevenue),
+
             lastPayoutAt: wallet.updatedAt
-        });
 
-    } catch (err) {
-
-        res.status(500).json({
-            success: false,
-            message: err.message
         });
 
     }
+
+    catch (err) {
+
+        res.status(500).json({
+
+            success: false,
+
+            message: err.message
+
+        });
+
+    }
+
 };
